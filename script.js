de57e16fa4bb03b2d43fd52f997c8c57f787633a
WNMDIVA <roblox.meow268@gmail.com>
2026-08-15 21:26:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-// =========================================================
-// 1. PREVENT AUTO-SCROLL ON REFRESH
-// =========================================================
-
-// Disable browser scroll restoration so it doesn't remember old scroll positions
-if ('scrollRestoration' in history) {
-  history.scrollRestoration = 'manual';
-}
-
-// Jump immediately to the top
-window.scrollTo(0, 0);
-
-// Remove any hash (#ocs, etc.) left in the address bar on refresh
-if (window.location.hash) {
-  history.replaceState(null, null, window.location.pathname);
-}
 
 
 // =========================================================
